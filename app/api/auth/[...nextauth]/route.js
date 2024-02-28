b9bbc02fecec65f25378892f1f1d4c075a71b3fe
the2MinEngineer <ifeanyiemmanueljoseph@gmail.com>
2024-02-28 01:18:31 +0100

    change auth options to next auth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,7 +4,7 @@ import connectDB from "../../../../utils/connect";
 import bcrypt from "bcrypt";
 import User from "@/app/(models)/User";
 
-export const authOptions = {
+export default NextAuth = {
 	pages: {
 		signIn: "/signin",
 		error: "/signin",
@@ -89,6 +89,3 @@ export const authOptions = {
 	secret: process.env.NEXTAUTH_SECRET,
 	debug: process.env.NODE_ENV === "development",
 };
-
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
